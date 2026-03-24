aa98e7a2c0ffddecef483de2561b3b5ab46f502b
vmv18 <Marinka.vlasovav@gmail.com>
2026-03-24 21:48:13 +0300

    fix import paths

--- src/components/organisms/AddStudentForm.jsx
+++ src/components/organisms/AddStudentForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Input from '../../atoms/Input/Input';
-import Button from '../../atoms/Button/Button';
+import Input from '../atoms/Input/Input';
+import Button from '../atoms/Button/Button';
 import styles from './AddStudentForm.module.css';
 
 const AddStudentForm = ({ onAddStudent }) => {
