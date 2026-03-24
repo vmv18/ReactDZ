ef370e4e816d5a2eb73164f723b1be0d5a706634
vmv18 <Marinka.vlasovav@gmail.com>
2026-03-24 21:11:00 +0300

    пр1

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{
+      backgroundColor: '#282c34',
+      color: '#fff',
+      padding: '15px',
+      textAlign: 'center',
+      borderRadius: '8px',
+      marginBottom: '20px'
+    }}>
+      <h2>Це новий Header компонент</h2>
+    </header>
+  );
+}
+
+export default Header;
